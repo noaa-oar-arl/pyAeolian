945881283a14b145d525079cdb51c9fa1dddc628
zmoon <zmoon92@gmail.com>
2022-10-17 12:34:25 -0600

    Clean up setup.py a bit

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from numpy.distutils.core import Extension
 
-ext2 = Extension(name="aeolian", sources=["pyaeolian/aeolian.pyf", "pyaeolian/aeolian.F90"])
+ext = Extension(name="aeolian", sources=["pyaeolian/aeolian.pyf", "pyaeolian/aeolian.F90"])
 
 if __name__ == "__main__":
     from numpy.distutils.core import setup
@@ -8,12 +8,11 @@ if __name__ == "__main__":
     setup(
         name="pyAeolian",
         version="0.1",
-        description="Wrapper around the NOAA ARL aeolian developmental parameterizations",
+        description="Wrapper around the NOAA ARL Aeolian developmental parameterizations",
         author="Barry D. Baker",
         lisense="MIT",
         author_email="barry.baker@noaa.gov",
-        source=["pyaeolian"],
         packages=["pyaeolian"],
-        ext_modules=[ext2],
-        install_requires=["numpy"],
+        ext_modules=[ext],
+        install_requires=["numpy", "xarray"],
     )
